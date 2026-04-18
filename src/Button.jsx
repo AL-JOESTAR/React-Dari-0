27caa5408bb417f21b7964adb677b00c35ee0c48
Sainzel <cppnaf@gmail.com>
2026-04-18 15:42:10 +0700

    use state

--- src/Button.jsx
+++ src/Button.jsx
@@ -1,11 +1,20 @@
+import { useState } from "react";
+
 function Button (){
 
+    const [counter, setCounter] = useState(0);
+
+    let newCounter = 0;
+
 function clickHandler() {
-    console.log(`saya diklik`);
+    newCounter = counter + 1;
+    console.log(`saya diklik ${newCounter}`);
+    setCounter(newCounter)
+
 }
 
     return (
-        <button onClick={clickHandler}>klik bray</button>
+        <button onClick={clickHandler}>klik bray {counter}</button>
     )
 }
 
