490ca8e85a4bb77df60f612159a5d7ae4b277323
Sainzel <cppnaf@gmail.com>
2026-04-20 22:59:54 +0700

    useMemo

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,24 @@
-import Button from "./Button"
+import { useMemo, useState } from "react"
+
 
 function App(){
+  const [likeCounter, setLikeCounter] = useState(0);
+  const [subcribeCounter, setSubscribeCounter] = useState(0);
+
+  function pesanLike(){
+    console.log('pesan like render');
+    return (likeCounter < 10) ? 'like kurang dari 10' : 'gg bang'
+  }
   
+  const displatPesanLike = useMemo(()=> pesanLike(),[likeCounter]);
   return (
   <>
-    <Button/>
+    <p>
+      <button onClick={()=>setLikeCounter(likeCounter + 1)}>{likeCounter} Like</button> {displatPesanLike}
+    </p>
+    <p>
+      <button onClick={()=>setSubscribeCounter(subcribeCounter + 1)}>{subcribeCounter} Subscribe</button>
+    </p>
   </>
 )
 }
